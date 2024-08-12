a33bd4ef8f09fb39a1dbfe7db270aba233089e39
Sanjay Singh <sanjaysinghg2u@gmail.com>
2024-08-13 00:02:56 +0530

    dependency injection

--- src/main/java/com/sanjay/demoApplication/DemoApplication.java
+++ src/main/java/com/sanjay/demoApplication/DemoApplication.java
@@ -2,12 +2,18 @@ package com.sanjay.demoApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+		Dev obj = context.getBean(Dev.class);
+
+		obj.build();
 	}
 
 }
---
+++ src/main/java/com/sanjay/demoApplication/Dev.java
@@ -0,0 +1,12 @@
+package com.sanjay.demoApplication;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build()
+    {
+        System.out.println("Hello World!");
+    }
+}
